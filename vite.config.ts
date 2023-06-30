a8d77885ed3e8f97b835c7c2061d247215d9c700
renan5g <renansilva.dev5g@gmail.com>
2023-06-30 13:21:35 -0300

    feat: start challenge

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+/// <reference types="vitest" />
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  plugins: [tsconfigPaths()],
+  test: {
+    globals: true,
+  },
+});
