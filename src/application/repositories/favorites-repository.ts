31e142be7e741c6b620178cdf4f553f3089456e2
renan5g <renansilva.dev5g@gmail.com>
2023-06-30 13:24:22 -0300

    feat: create favorites repository interface

---
+++ src/application/repositories/favorites-repository.ts
@@ -0,0 +1,10 @@
+import { PaginationParams } from '@core/repositories/pagination-params';
+import { Favorite } from '@domain/entities/favorite';
+
+export abstract class FavoritesRepository {
+  abstract findByHero(hero: string): Promise<Favorite | null>;
+  abstract findMany(params: PaginationParams): Promise<Favorite[]>;
+  abstract create(favorite: Favorite): Promise<void>;
+  abstract save(favorite: Favorite): Promise<void>;
+  abstract delete(favorite: Favorite): Promise<void>;
+}
